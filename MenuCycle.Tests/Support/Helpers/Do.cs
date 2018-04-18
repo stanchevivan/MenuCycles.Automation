4ab9cc431d3beff47aeadf65bedb9e9630418516
Ivan Stanchev <Ivan.Stanchev@fourth.com>
2018-04-18 09:03:02 -0700

    - add missing files

---
+++ MenuCycle.Tests/Support/Helpers/Do.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+
+namespace MenuCycle.Tests
+{
+    public static class Do_Extension
+    {
+        public static Do Do(this IWebElement element)
+        {
+            return new Do(element);
+        }
+    }
+
+    public class Do
+    {
+        private IWebElement webElement;
+
+        public Do(IWebElement element)
+        {
+            webElement = element;
+        }
+
+        public void Click()
+        {
+            webElement.Click();
+        }
+
+        public void JavaClick(IWebDriver driver)
+        {
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].Cli‌​ck()", webElement);
+        }
+
+        public void HighlightAndClick(IWebDriver driver)
+        {
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].style.border='3px solid lightgreen'", webElement);
+            System.Threading.Thread.Sleep(200);
+            webElement.Click();
+        }
+
+        public void ClearAndSendKeys(string text)
+        {
+            webElement.Clear();
+            webElement.SendKeys(text);
+        }
+
+        public void Clear()
+        {
+            webElement.Clear();
+        }
+    }
+}
