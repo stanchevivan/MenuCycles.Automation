a8cf106d36d739373f9390586085542a0b32c045
stanchevivan <stanchev.ivan@abv.bg>
2018-09-10 12:46:45 +0300

    - add appium server start/stop to avoid conflicts with other projects

---
+++ MenuCycle.Tests/Support/AppiumHooks.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Fourth.Automation.Framework.Core;
+using Fourth.Automation.Framework.Mobile;
+using Fourth.Automation.Framework.Mobile.Appium;
+using Fourth.Automation.Framework.Mobile.Resolvers;
+using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+
+namespace MenuCycle.Tests.Support
+{
+    [Binding]
+    public sealed class AppiumHooks
+    {
+        [Binding]
+        public sealed class AppiumServerForTestRun
+        {
+            private static readonly AppiumDebugServers appiumServers;
+
+            static AppiumServerForTestRun()
+            {
+                appiumServers = new AppiumDebugServers(
+                    new TerminalCommand[] {
+                    new AppiumServer(DriverFactory.DriverInfo),
+                    new IOSWebkitDebugProxy(DriverFactory.DriverInfo) });
+            }
+
+            [BeforeTestRun]
+            public static void BeforeTestRun()
+            {
+                DriverFactory.Resolvers.Add(new AndroidResolver());
+                DriverFactory.Resolvers.Add(new IOSResolver());
+                appiumServers.Start(DriverFactory.DriverInfo);
+            }
+
+            [AfterTestRun]
+            public static void AfterTestRun()
+            {
+                appiumServers.Stop();
+            }
+        }
+    }
+}
