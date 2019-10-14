8d75b39338190e51d6674ea597e5e6db559bf510
Hristina Nikolova <Hristina.Nikolova@fourth.com>
2019-10-14 17:08:55 +0300

    MassUpdateSearchResultCard adding selectors and methods

--- MenuCycle.Tests/PageObjects/MassUpdate/MassUpdateSearchResultCard.cs
+++ MenuCycle.Tests/PageObjects/MassUpdate/MassUpdateSearchResultCard.cs
@@ -1,12 +1,46 @@
 ﻿using System;
 using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
 
 namespace MenuCycle.Tests.PageObjects.MassUpdate
 {
-    public class MassUpdateResult :MenuCyclesBasePage
+    public class MassUpdateSearchResultCard : MenuCyclesBasePage
     {
-        public MassUpdateResult(IWebDriver webDriver) : base(webDriver)
+        public MassUpdateSearchResultCard(IWebElement parent,IWebDriver webDriver) : base(webDriver)
         {
+			PageFactory.InitElements(parent, this);
+		}
+
+		[FindsBy(How = How.CssSelector, Using = ".search-results-card__cell:last-of-type")]
+        private IWebElement RecipeCardName { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = ".search-results-card__cell .mc-checkbox")]
+        private IWebElement RecipeCardCheckbox { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = ".search-results-card .search-results-card__arrow-icon")]
+        private IWebElement OccurrencesArrow { get; set; }
+
+        public string RecipeName => RecipeCardName.Text;
+
+        public void ClickOccurencesArrow()
+        {
+            OccurrencesArrow.Click();
+        }
+
+        public void SelectRecipeCard()
+        {
+            if (!RecipeCardCheckbox.Selected)
+            {
+                RecipeCardCheckbox.Click();
+            }
+        }
+
+        public void DeselectRecipeCard()
+        {
+            if (RecipeCardCheckbox.Selected)
+            {
+                RecipeCardCheckbox.Click();
+            }
         }
     }
 }
